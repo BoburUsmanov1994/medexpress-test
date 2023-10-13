2142a159b61c59299733c5cab2cca1dbf7239791
Bobur <boburusmanov1994@gmail.com>
2023-10-13 18:25:19 +0500

    fix

--- config-overrides.js
+++ config-overrides.js
@@ -1,6 +1,15 @@
 const ModuleScopePlugin = require('react-dev-utils/ModuleScopePlugin');
 
 module.exports = function override(config, env) {
+    Object.assign(fallback, {
+        "crypto": require.resolve("crypto-browserify"),
+        "stream": require.resolve("stream-browserify"),
+        "assert": require.resolve("assert"),
+        "http": require.resolve("stream-http"),
+        "https": require.resolve("https-browserify"),
+        "os": require.resolve("os-browserify"),
+        "url": require.resolve("url")
+    })
     config.resolve.plugins = config.resolve.plugins.filter(plugin => !(plugin instanceof ModuleScopePlugin));
 
     return config;
